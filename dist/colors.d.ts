962ff9c4e7d56db1a60f8fd361bf70a1a70d4338
Sathmin-Januth <sathminj1@gmail.com>
2022-08-05 12:36:27 +0530

    first commit

---
+++ dist/colors.d.ts
@@ -0,0 +1,6 @@
+import rawDark from './dark';
+export declare type Scheme = typeof rawDark;
+export declare const dark: Scheme;
+export declare const light: Scheme;
+export declare const mirage: Scheme;
+export { alphaBlend } from './color';
---
+++ dist/colors.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dark_1 = require("./dark");
+const light_1 = require("./light");
+const mirage_1 = require("./mirage");
+exports.dark = dark_1.default;
+exports.light = light_1.default;
+exports.mirage = mirage_1.default;
+var color_1 = require("./color");
+exports.alphaBlend = color_1.alphaBlend;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29sb3JzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbG9ycy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLGlDQUE0QjtBQUM1QixtQ0FBOEI7QUFDOUIscUNBQWdDO0FBSW5CLFFBQUEsSUFBSSxHQUFXLGNBQU8sQ0FBQTtBQUN0QixRQUFBLEtBQUssR0FBVyxlQUFRLENBQUE7QUFDeEIsUUFBQSxNQUFNLEdBQVcsZ0JBQVMsQ0FBQTtBQUV2QyxpQ0FBb0M7QUFBM0IsNkJBQUEsVUFBVSxDQUFBIn0=
