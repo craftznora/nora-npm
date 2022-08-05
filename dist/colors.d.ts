746e077bcd8a9816ae0a69cdbe2a500d807f659a
Sathmin Januth <89331370+Sathmin-Januth@users.noreply.github.com>
2022-08-05 13:45:02 +0530

    Delete dist directory

--- dist/colors.d.ts
+++
@@ -1,6 +0,0 @@
-import rawDark from './dark';
-export declare type Scheme = typeof rawDark;
-export declare const dark: Scheme;
-export declare const light: Scheme;
-export declare const mirage: Scheme;
-export { alphaBlend } from './color';
